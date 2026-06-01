929878f0a0299ace35e411cedebeecd0ab7e6c25
rahul-codingexpo <rahul.codingexpo@gmail.com>
2026-06-01 19:04:02 +0530

    website contact form updated

--- getizy/files/assets/inc/sendemail.php
+++ getizy/files/assets/inc/sendemail.php
@@ -1,8 +1,8 @@
 <?php
 
 // Define some constants
-define( "RECIPIENT_NAME", "John Doe" );
-define( "RECIPIENT_EMAIL", "mail@mail.com" );
+define( "RECIPIENT_NAME", "Coding Expo" );
+define( "RECIPIENT_EMAIL", "sales@codingexpo.org" );
 
 // Read the form values
 $success = false;
